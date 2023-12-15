5b0012074c7926228051db9490b0b2e1e0dfc440
guanwanxiao <guanwanxiao@mbse.com.cn>
2023-12-15 17:47:15 +0800

    feat: 参数装饰器添加元数据

--- src/decorator.ts
+++ src/decorator.ts
@@ -1,3 +1,5 @@
+import "reflect-metadata";
+
 /*** 
  * 方法装饰器
  */
@@ -6,7 +8,7 @@ function factor(params: any) {
     // class 类中的方法装饰器接受三个参数，对传入的 jump 函数进行处理
     return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
         // 限制函数修改
-        descriptor.writable = false
+        // descriptor.writable = false
     }
 }
 
@@ -19,11 +21,35 @@ function factor1(params:any) {
     }
 }
 
+
+const formatMetadataKey = Symbol("format");
+
+/**
+ * 给参数装饰器添加一些元数据
+ */
+function format(formatString: string) {
+    return Reflect.metadata(formatMetadataKey, formatString);
+}
+
+
 @factor1('类装饰器')
 export class Foo {
+    // 给类属性添加元数据
+    @format("Hello, %s")
+    greeting!: string;
+     _y = 'y'
+
     @factor('这里是静态方法')
     static say() {
         console.log('say')
+    }
+    @factor('访问器静态属性 x')
+    static get x () {
+        return 'x'
+    }
+    @factor('访问器属性 y')
+    get y () {
+        return this._y
     }
     // 首先会执行 factor 函数，并且将传递参数给 factor
     @factor('我要调用 factor 函数了')
@@ -31,3 +57,7 @@ export class Foo {
         console.log('i want to jump')
     }
 }
+const foo = new Foo()
+// 读取类上属性的元数据语法： Reflect.getMetadata(metadataKey, target[, propertyKey])
+const result = Reflect.getMetadata(formatMetadataKey, foo, "greeting");
+console.log('result: ', result)
